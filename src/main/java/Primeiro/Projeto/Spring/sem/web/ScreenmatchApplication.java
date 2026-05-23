ebeceaa654c06c56187a0733eb08ff06f0013f3a
Gabriel Castro <gabrielwdc@hotmail.com>
2026-05-23 11:00:29 -0300

    feat: adição da classe episódio e integraçao com a classe principal para buscar os 5 tops episódios e filtrar a busca por data

--- src/main/java/Primeiro/Projeto/Spring/sem/web/ScreenmatchApplication.java
+++ src/main/java/Primeiro/Projeto/Spring/sem/web/ScreenmatchApplication.java
@@ -1,8 +1,6 @@
 package Primeiro.Projeto.Spring.sem.web;
 
-import Primeiro.Projeto.Spring.sem.web.model.DadosSerie;
-import Primeiro.Projeto.Spring.sem.web.service.ConsumoApi;
-import Primeiro.Projeto.Spring.sem.web.service.ConverteDados;
+import Primeiro.Projeto.Spring.sem.web.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,13 +14,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		ConsumoApi consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=338f260e");
-//		System.out.println(json);
-//		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
--- src/main/java/Primeiro/Projeto/Spring/sem/web/model/DadosTemporada.java
+++ src/main/java/Primeiro/Projeto/Spring/sem/web/model/DadosTemporada.java
@@ -1,4 +1,12 @@
 package Primeiro.Projeto.Spring.sem.web.model;
 
-public record DadosTemporada() {
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosTemporada(@JsonAlias("Season") Integer numero,
+                             @JsonAlias("Episodes") List<DadosEpisodio> episodios) {
+
 }
